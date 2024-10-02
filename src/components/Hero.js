f39942f16516b52b0e25ab822752be62ec5cdd15
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-10-02 12:49:23 +0100

    Quick updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
 
         <div className='wrapper'>
             <section class="hero-text">
-                <h1>Welcome to Electrical Emergency Limited</h1>
+                <h1>Welcome to 24/7 ASAP Electrical Emergency Limited</h1>
                 <p>We guarantee that all our electrical installations are not only safe and efficient but also fully compliant with the latest standards</p>
                 <a>View Services</a>
             </section>
