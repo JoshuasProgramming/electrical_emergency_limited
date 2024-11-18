2413825fe69efba8e770ec6cdbe6af3b749b9a2b
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-11-18 12:32:01 +0000

    Whatsapp

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import whatsapp from '../images/social_media_icons/whatsapp.png';
+import phone from '../images/social_media_icons/phone.png';
 
 function SocialLinks() {
 
@@ -11,9 +12,15 @@ function SocialLinks() {
     <div className='social-navigation-links'>
         <ul className='social-navigation'>
             {/* <a href={"whatsapp://send?text" + message + number}> */}
-            <a href={"https://wa.me/" + number}>
+            <a href={"https://wa.me/" + number} title='WhatsApp us'>
                 <li>
                     <img src={whatsapp}></img>
+                </li>
+            </a>
+
+            <a href={"tel:+447487411363"} title='Call Us'>
+                <li>
+                    <img src={phone}></img>
                 </li>
             </a>
         </ul>
